0f415b46d215716607de0ad47144542aac294dbb
Anti-Mage <jpottagarda@gmail.com>
2023-11-17 14:48:19 +0800

    Added initial files

---
+++ src/main/java/Employee/Employee.java
@@ -0,0 +1,29 @@
+package Employee;
+
+
+import java.time.LocalDate;
+
+public class Employee {
+    
+    private  String name;
+    private  double salary;
+    private  LocalDate hireDate;
+
+    public Employee(String name, double salary, LocalDate hireDate) {
+        this.name = name;
+        this.salary = salary;
+        this.hireDate = hireDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+}
