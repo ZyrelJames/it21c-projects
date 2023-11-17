cfede9cc0970b65a43809343d2dcbf4135ab2e66
Anti-Mage <jpottagarda@gmail.com>
2023-11-17 14:59:09 +0800

    Added initial files

---
+++ src/main/Student/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    
+    
+    public static void main(String[] args) {
+      ZyrelJames g = new ZyrelJames ();
+       
+       System.out.println("Enter your Name: ");
+       g.setName();
+       System.out.println("Enter your Section: ");
+       g.setSection();
+       System.out.println("Enter your Motto: ");
+       g.setMotto();
+       System.out.println("\nSTUDENT'S PROFILE!\n");
+       g.Info();
+        
+    }
+    
+}
