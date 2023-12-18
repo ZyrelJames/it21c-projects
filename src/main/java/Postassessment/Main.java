065971a420d4f42fafadddfdea3b8e39a93c2aaa
Anti-Mage <jpottagarda@gmail.com>
2023-12-18 12:07:05 +0800

    Added initial files

---
+++ src/main/java/Postassessment/Main.java
@@ -0,0 +1,14 @@
+package postassessment;
+
+public class Main {
+         
+    public static void main(String [] args){
+        School school_name = new School("Northern Bukidnon State College");
+        school_name.school();
+        Programs program_name = new Programs("");
+        program_name.ProgramName();
+	
+ 		
+	}
+    
+}
---
+++ src/main/java/Postassessment/School.java
@@ -0,0 +1,24 @@
+package postassessment;
+public class School{
+	
+    
+    String name;
+    
+    School(String name) {
+        this.name = name;
+        
+    }
+    
+    void school(){
+        System.out.println(name + "(NBSC)");
+    }
+    
+    void schoolgyapon (){
+        System.out.println(name + "BSBA");
+         
+    }
+    
+    void SchoolName(){
+        System.out.println("BSIT");
+    }
+}
